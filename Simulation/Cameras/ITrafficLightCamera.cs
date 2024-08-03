e7e900dd44f79f3878b4273ee0dea3fb48009477
16Shadows <egor.0.00@mail.ru>
2024-08-03 11:42:58 +0500

    feat: Интерфейс камеры светофора.
    Описывает релевантные для светофора свойства - длину очереди и событие изменения длины очереди.

---
+++ Simulation/Cameras/ITrafficLightCamera.cs
@@ -0,0 +1,24 @@
+﻿namespace Simulation.Cameras
+{
+    /// <summary>
+    /// Делагат события изменения размера очереди перед камерой (<see cref="ITrafficLightCamera"/>)
+    /// </summary>
+    /// <param name="sender"></param>
+    public delegate void TrafficLightCameraQueueSizeChanged(ITrafficLightCamera sender);
+
+    /// <summary>
+    /// Интерфейс камеры светофора.
+    /// </summary>
+    public interface ITrafficLightCamera
+    {
+        /// <summary>
+        /// Текущий зафиксированный размер очереди перед камерой.
+        /// </summary>
+        int QueueSize { get; }
+
+        /// <summary>
+        /// Событие изменения размера очереди перед камерой.
+        /// </summary>
+        event TrafficLightCameraQueueSizeChanged QueueSizeChanged;
+    }
+}
