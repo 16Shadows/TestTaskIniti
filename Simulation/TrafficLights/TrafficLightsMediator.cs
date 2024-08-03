916d621c99e6f4d7034952a597d7ed91ef1c76aa
16Shadows <egor.0.00@mail.ru>
2024-08-03 14:07:21 +0500

    feat: Канал связи светофоров
    Добавлена реализация канала связи светофоров в виде простого медиатора.

---
+++ Simulation/TrafficLights/TrafficLightsMediator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Concurrent;
+
+namespace Simulation.TrafficLights
+{
+    /// <summary>
+    /// Реализация канала связи между светофорами в виде просто медиатора.
+    /// </summary>
+    public class TrafficLightsMediator : ITrafficLightChannel
+    {
+        /// <summary>
+        /// Список всех зарегистрированных светофоров.
+        /// </summary>
+        private readonly ConcurrentDictionary<int, ITrafficLight> _TrafficLights = new();
+
+        /// <summary>
+        /// Отправить сообщение светофору с заданным id.
+        /// </summary>
+        /// <param name="receiverID">ID светофора, которому нужно доставить сообщение.</param>
+        /// <param name="message">Сообщение.</param>
+        /// <exception cref="KeyNotFoundException">Бросается, если светофора с таким ID нет в системе.</exception>
+        public void DispatchMessage(int receiverID, TrafficLightMessageBase message)
+        {
+            if (!_TrafficLights.TryGetValue(receiverID, out ITrafficLight? light) || light == null)
+                throw new KeyNotFoundException($"Invalid traffic light id - {receiverID}");
+
+            light.EnqueueMessage(message);
+        }
+
+        /// <summary>
+        /// Добавить светофор в роли приёмника в канал связи.
+        /// </summary>
+        /// <param name="trafficLight">Светофор, который нужно добавить.</param>
+        /// <exception cref="ArgumentException">Бросается, если светофор с таким ID уже добавлен.</exception>
+        public void Attach(ITrafficLight trafficLight)
+        {
+            if (!_TrafficLights.TryAdd(trafficLight.ID, trafficLight))
+                throw new ArgumentException($"A traffic light with the same ID ({trafficLight.ID}) has already been added", nameof(trafficLight));
+        }
+    }
+}
