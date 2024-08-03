3d84de30cb7abd3c238d217772fcdc092265bc74
16Shadows <egor.0.00@mail.ru>
2024-08-03 16:17:10 +0500

    feat: Реализована FSM светофоров
    Реалиована Finite State Machine для конфигураций светофоров на перекрёстке.
    
    Класс TrafficLightSetStateMessage реализует сообщение о необходимости изменить состояние светофора.
    Класс TrafficLightQueueSizeChangedMessage реализует сообщение об изменении размера очереди светофора.
    Класс TrafficLightSlaveBrain реализует компонент управления подчинённым светофором, который изменяет своё состояние при получении сообщения SetState и отправляет длину очереди главному светофору.
    Класс TrafficLightMasterBrain реализует компонент управления главным светофором, который использует FSM (определяющей лучшее состояние по оценке состояния) для определения состояния всех светофоров на перекрёстке.
    Класс TrafficLightsState реализует состояние FSM с методами для подсчёта оценки состояния и применения состояния.

---
+++ Algorithm/TrafficLightQueueSizeChangedMessage.cs
@@ -0,0 +1,20 @@
+﻿using Simulation.TrafficLights;
+
+namespace Algorithm
+{
+	/// <summary>
+	/// Сообщение об изменении размера очереди светофора.
+	/// </summary>
+	public class TrafficLightQueueSizeChangedMessage : TrafficLightMessageBase
+	{
+		/// <summary>
+		/// Новый размер очереди светофора.
+		/// </summary>
+		public int NewSize { get; }
+
+		public TrafficLightQueueSizeChangedMessage(int senderID, int newSize) : base(senderID)
+		{
+			NewSize = newSize;
+		}
+	}
+}
---
+++ Algorithm/TrafficLightSetStateMessage.cs
@@ -0,0 +1,20 @@
+﻿using Simulation.TrafficLights;
+
+namespace Algorithm
+{
+	/// <summary>
+	/// Сообщение о необходимости изменить состояние светофора.
+	/// </summary>
+	public class TrafficLightSetStateMessage : TrafficLightMessageBase
+	{
+		/// <summary>
+		/// Должен ли светофор пропускать сущности в новом состоянии.
+		/// </summary>
+		public bool CanBePassed { get; }
+
+		public TrafficLightSetStateMessage(int senderID, bool canBePassed) : base(senderID)
+		{
+			CanBePassed = canBePassed;
+		}
+	}
+}
