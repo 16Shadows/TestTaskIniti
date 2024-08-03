1b999a72e5d42b1efd3bbd58fd80d51a2b25b14a
16Shadows <egor.0.00@mail.ru>
2024-08-03 11:40:48 +0500

    feat: Интерфейс дороги.
    Добавлен интерфейс дороги (очереди на подходе к перекрёстку).

---
+++ Simulation/Roads/IRoad.cs
@@ -0,0 +1,18 @@
+﻿namespace Simulation.Roads
+{
+	/// <summary>
+	/// Интерфейс дороги (очереди на подходе к перекрёстку)
+	/// </summary>
+	public interface IRoad
+	{
+		/// <summary>
+		/// Текущий размер очереди.
+		/// </summary>
+		int QueueSize { get; }
+		
+		/// <summary>
+		/// Убрать одну сущность из очереди
+		/// </summary>
+		void PopQueue();
+	}
+}
